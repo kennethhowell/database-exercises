15be8712216a5f6ec0fc9d96f7f4b5919a5d907b
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-12-19 11:55:17 -0600

    aliases work complete

---
+++ aliases_exercises.sql
@@ -0,0 +1,7 @@
+use employees;
+
+SELECT concat(last_name, ', ', first_name) AS 'full_name' FROM employees limit 10;
+
+
+
+SELECT concat(emp_no, ' - ', last_name, ', ', first_name) AS 'full_name', birth_date AS 'D.O.B' FROM employees limit 10;
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT title FROM titles;
 
 SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE 'E%' and last_name like '%e' GROUP BY last_name ORDER BY COUNT(*) DESC;
 
-SELECT gender, MAX(birth_date), COUNT(*) FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender ORDER BY COUNT(*) DESC;
+SELECT gender, COUNT(*) FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender ORDER BY COUNT(*) DESC;
 
 
 SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%' and last_name like '%e' GROUP BY last_name, first_name;

a1431b76c1c67d8cdcfbca60f6cb0732c07f5521
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-01-06 16:46:50 -0600

    after demo w/ notes

--- database_design.sql
+++ database_design.sql
@@ -1,64 +1,64 @@
-CREATE DATABASE database_design_db;
-
-use database_design_db;
-
-CREATE TABLE users(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(20) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(18) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE advertisements(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    ad_title VARCHAR(20) NOT NULL,
-    ad_desc VARCHAR(200) NOT NULL,
-    user_id INT UNSIGNED DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-
-CREATE TABLE categories(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
-    category_title VARCHAR(40)
-);
-
-CREATE TABLE advertisement_category(
-    advertisement_id INT UNSIGNED,
-    category_id INT UNSIGNED,
-    FOREIGN KEY (advertisement_id) REFERENCES advertisements(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
-
-INSERT INTO users(username, email, password)
-    VALUES ('kenhow', 'ken@ken.com', 'kennyrocks'),
-           ('miggarc', 'miguel@hotman.com', 'rockinrocker'),
-           ('melinnn', 'melinda@gmailss.com', 'melmelmel');
-
-SELECT * from users;
-
-INSERT INTO categories(category_title)
-    VALUES ('home'),
-           ('tech'),
-           ('apartments'),
-           ('work'),
-           ('free'),
-           ('for sale');
-
-SELECT * from categories;
-
-DESCRIBE advertisements;
-
-INSERT INTO advertisements(ad_title, ad_desc, user_id)
-    VALUES ('sofa', 'really cushy',2);
-
-SELECT * from advertisements;
-
-INSERT INTO advertisement_category(advertisement_id, category_id)
-    VALUES (4,1);
+# CREATE DATABASE database_design_db;
+#
+# use database_design_db;
+#
+# CREATE TABLE users(
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     username VARCHAR(20) NOT NULL,
+#     email VARCHAR(30) NOT NULL,
+#     password VARCHAR(18) NOT NULL,
+#     PRIMARY KEY (id)
+# );
+#
+# CREATE TABLE advertisements(
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     ad_title VARCHAR(20) NOT NULL,
+#     ad_desc VARCHAR(200) NOT NULL,
+#     user_id INT UNSIGNED DEFAULT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+#
+#
+# CREATE TABLE categories(
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     PRIMARY KEY (id),
+#     category_title VARCHAR(40)
+# );
+#
+# CREATE TABLE advertisement_category(
+#     advertisement_id INT UNSIGNED,
+#     category_id INT UNSIGNED,
+#     FOREIGN KEY (advertisement_id) REFERENCES advertisements(id),
+#     FOREIGN KEY (category_id) REFERENCES categories(id)
+# );
+#
+# INSERT INTO users(username, email, password)
+#     VALUES ('kenhow', 'ken@ken.com', 'kennyrocks'),
+#            ('miggarc', 'miguel@hotman.com', 'rockinrocker'),
+#            ('melinnn', 'melinda@gmailss.com', 'melmelmel');
+#
+# SELECT * from users;
+#
+# INSERT INTO categories(category_title)
+#     VALUES ('home'),
+#            ('tech'),
+#            ('apartments'),
+#            ('work'),
+#            ('free'),
+#            ('for sale');
+#
+# SELECT * from categories;
+#
+# DESCRIBE advertisements;
+#
+# INSERT INTO advertisements(ad_title, ad_desc, user_id)
+#     VALUES ('sofa', 'really cushy',2);
+#
+# SELECT * from advertisements;
+#
+# INSERT INTO advertisement_category(advertisement_id, category_id)
+#     VALUES (4,1);
 
 SELECT * from users;
 SELECT * from advertisements;
@@ -104,3 +104,4 @@ WHERE user_id IN (
     WHERE username = 'miggarc'
     )
 
+# fifty chars for pw, watch redundant titles, watch use of DESC

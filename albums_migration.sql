d5ceccfbc310e23ccaa813264b914f06522bbf57
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-01-06 10:18:08 -0600

    working through SQL morning warmup

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,15 @@
-USE codeup_test_db;
+-- USE codeup_test_db;
+--
+-- DROP TABLE IF EXISTS albums;
+--
+-- CREATE TABLE albums (
+--     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     artist_name VARCHAR(50),
+--     record_name VARCHAR(50),
+--     release_date int,
+--     sales float,
+--     genre VARCHAR(100),
+--     PRIMARY KEY (id)
+-- );
+CREATE DATABASE music_db;
 
-DROP TABLE IF EXISTS albums;
-
-CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(50),
-    record_name VARCHAR(50),
-    release_date int,
-    sales float,
-    genre VARCHAR(100),
-    PRIMARY KEY (id)
-);
--- scratchpad_exercise.sql
+++ scratchpad_exercise.sql
@@ -1,22 +1,35 @@
-use codeup_test_db;
+# use codeup_test_db;
+#
+#
+# describe albums;
+#
+# alter table albums add unique (artist_name, record_name);
+#
+# SELECT * from albums;
+#
+# show create table albums;
+#
+# use employees;
+#
+# select concat(first_name, ', ', last_name), emp_no from employees
+#
+# select * from dept_manager where emp_no = 110022;
+# select * from dept_emp where emp_no = 110022;
+#
+# select * from employees where emp_no = 110022;
+#
+# select * from departments where dept_no = 'd001';
+#
+
+use music_db;
+
+SELECT COUNT(*) from DeimosPlayer;
 
 
-describe albums;
+SELECT * from DeimosPlayer ORDER BY duration DESC LIMIT 3;
 
-alter table albums add unique (artist_name, record_name);
+SELECT artist_name, COUNT(*) from DeimosPlayer GROUP BY artist_name ORDER BY COUNT(*) DESC;
 
-SELECT * from albums;
+SELECT song_name, artist_name FROM DeimosPlayer WHERE song_name LIKE 'non mauris morbi non';
 
-show create table albums;
-
-use employees;
-
-select concat(first_name, ', ', last_name), emp_no from employees
-
-select * from dept_manager where emp_no = 110022;
-select * from dept_emp where emp_no = 110022;
-
-select * from employees where emp_no = 110022;
-
-select * from departments where dept_no = 'd001';
-
+SELECT MAX(LENGTH(duration)) FROM DeimosPlayer;
